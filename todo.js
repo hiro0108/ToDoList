8dcb02a59121073e72228236aaf6febe39e25b71
hiro0108 <71159075+hiro0108@users.noreply.github.com>
2021-02-10 19:01:58 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,37 +1,52 @@
 var count = 0;
 var addCharSet = [];
+
+$(document).ready(function(){
+    $("#myTable").tablesorter({
+        headers: {
+            0: { sorter: false},
+            1: { sorter: false},
+            2: { sorter: "digit"}
+        }
+    });
+});
+
 function addList(idTodo, idDate){
     let todoLists = document.getElementById('todoLists');
     let newList = document.getElementById(idTodo);
     let date = document.getElementById(idDate);
-    if(checkNum(idDate) == 1){
-        var addChar = '<li id="listAll"><input type="checkbox" name="option"><div id= list contentEditable="true">　'+ newList.value +'　</div><div id=datelist contentEditable="true">（'+date.value+'）</div></li>';//htmlに挿入する文字列     
-        addCharSet[count] = addChar;//挿入する文字列を配列に格納(この配列をローカルストレージに保存)
-        localStorage.setItem('todoList',JSON.stringify(addCharSet));//配列ごとローカルストレージに保存
+    let year = document.getElementById("year");
+    if(checkNum(idDate) && checkNum('year') == 1){
+        var addChar = '<tr><td><input type="checkbox" name="option" id="check"></td><td contentEditable="true">'+ newList.value +'</td><td contentEditable="true">'+ year.value +'/'+ date.value +'</td></tr>';//htmlに挿入する文字列     
+        addCharSet[count] = addChar;
+        localStorage.setItem('todoList',JSON.stringify(addCharSet));
         todoLists.insertAdjacentHTML('beforeend', addCharSet[count]);
+        $("#myTable").trigger("update");
         newList.value = "";
         date.value = "";
         count = count + 1;
     }else{
-        let message = '期日は半角数字のみで入力してください(例:1225)';
+        let message = '年と期日は半角数字のみで入力してください(例:1225)';
         alert(message);
         date.value = "";
+        year.value = "";
     }
-};
+}
+
 
 function removeList(){
-    let options = document.getElementsByName('option');//optionはチェックボックスのID
+    let options = document.getElementsByName('option');
     var removeItem = JSON.parse(localStorage.getItem('todoList'));
     for(let i = 0; i < options.length; i++){
         if(options[i].checked){
-            options[i].parentElement.remove();//checkboxの親要素を削除
+            options[i].parentElement.parentElement.remove();
             removeItem.splice(i, 1);
             localStorage.setItem('todoList', JSON.stringify(removeItem));
         }
     }
 }
 
-function checkNum(idDate){//数字なら１それ以外なら０を返す
+function checkNum(idDate){
     let kijitu = document.getElementById(idDate);
     var kijituNum = Number(kijitu.value)
     if(isNaN(kijituNum) == false){
@@ -41,11 +56,6 @@ function checkNum(idDate){//数字なら１それ以外なら０を返す
     }
 }
 
-function save(key, data){
-    //var myData = document.getElementById(id).value
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
 function load(key){
     var outputData = localStorage.getItem(key);
     outputData = JSON.parse(outputData);
@@ -53,15 +63,5 @@ function load(key){
         todoLists.insertAdjacentHTML('beforeend', outputData[i]);
     }
     localStorage.setItem('todoList',JSON.stringify(outputData));
+    $("#myTable").trigger("update");
 }
-/*
-insertAdjacentHTML('挿入場所', 挿入文字列);
-->html上で位置を指定し、そこに文字列を挿入する
-innerHTMLは丸ごと差し替える時に使用する
-
-parentElement:親要素を取得する、今回はチェックボックスがチェックれた時に行ごと消すために親要素を取得した
-
-contentEditable="true"：これを書くことによって編集モードにすることが可能になる
-
-isNaN()はNumber()で"aaa"のような文字列の型変換した時にNaNと表示されるため、NaNかどうかを判定するための関数
- */
